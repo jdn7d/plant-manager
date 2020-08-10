1753fdc05c882308db83a02a1ca169c8df9a6437
Jessie <jssdnvn.com>
2020-08-09 21:51:01 -0500

    first commit

--- lib/plant_manager/cli.rb
+++ lib/plant_manager/cli.rb
@@ -1,51 +1,48 @@
 # Our CLI Controller
 # menus and main logic 
 class PlantManager::CLI
-
-    def call
-        
-        all_plants
-        menu
-      #  bye
-    end
-    def all_plants
-         guide_url =  "https://www.guide-to-houseplants.com/house-plants-encyclopedia-a-z.html"  #url
-        parsed_page = Nokogiri::HTML(open(guide_url))                                           #opening up the page  
-        urls = parsed_page.css("a").collect {|all| all.attribute("href").value}     
-        
-    @scraper = PlantManager::Scraper.new
-    @scraper.scraper
+    guide_url =  "https://www.guide-to-houseplants.com/house-plants-encyclopedia-a-z.html"
     
-    #@@plants
-       #@@plants = PlantManager::Plants.scrape_site
-       #@@plants.to_a.each.with_index(1) do |plant, i|
-       #puts "#{1}. #{plant.name}"
-    end
     def menu
-        input = nil
-            puts "Which houseplant are you are you curious about today? Type exit if you don't want to search for a plant. Type all to see all plants."
-            
-            while input != "exit" 
-             if input != " "
-               # selected_plant = @plants[input.to_i-1]
-            input = gets.strip.downcase
-            case input 
-            when "1"
-            puts "More info on deal 1..."
-           # puts "Is there another plant you would like to search for?"  
-            when "all"
-                puts "this is a list of all plants"
-                  all_plants
-            when "exit"
-                puts "Good luck keeping those plants alive!" 
-            else 
-                puts "Invalid entry"
-            end
-            end
-        def bye
-            
+        @input = nil        
+        plant = Plant.new(Scraper.scraper)
+    end
+    def call
+        puts "Which houseplant are you are you curious about today? Type exit if you don't want to search for a plant. Type all to see all plants."
+        while @input != "exit"
+            puts "Here is a list of all the houseplants."
+            PlantManager::Plant.all_plants
+            input = gets.strip
+        if @input == "all"
+            call
+        elsif @input == "exit"
+            exit
+        elsif @input.to_i.between?(1, PlantManager::Plant.all.count)
+            select_plant
+        else
+            puts "Invalid Entry"
+            call
+        end
+        if @input == "exit"
+            exit
+        end
         end
     end
+    def select_plant
+        selected_plant = Plant.find(@input)
+        puts "#{select_plant.name}:"
+
+        selected_plant.each_with_index do |plant, index|
+            puts ""
+      
+            puts "#{index+1}. #{plant.name}#{plant.water}#{plant.light}#{plant.fertilizer}"
+          end
+          
+    end
+    def exit
+        puts "Good luck keeping those plants alive!"
+    end
 end
-end
+
+
 
--- lib/plant_manager/plants.rb
+++ lib/plant_manager/plants.rb
@@ -2,21 +2,34 @@
 #require "nokogiri"
 
 class PlantManager::Plant
- 
-    #attr_accessor :name, :water, :light, :fertilizer, :url
-    @@all = []
 
-    def self.new_from_index_page
-    
-    end
-   
-    def self.scrape_site
-        
-        plants = []
-        plants << self.scrape
-        plants
-    end
-end
- 
+    attr_accessor :name, :water, :sun, :fertilizer, :url      #creating plant attributes
+    @@all = []                                                #creating a class variable for all plants
 
-    
+    def self.find(plant_number)                               #this is looking in the class variable for a plant 
+      @@all[plant_number.to_i-1]
+    end
+  
+    def initialize(name)
+      @name = name
+      @water = water 
+      @light = light 
+      @fertilizer = fertilizer 
+      @@all << self 
+
+    end
+  
+    def find_plant(plant_number)
+      self.plants[plant_number.to_i-1]
+    end
+  
+    def self.all
+      @@all
+    end
+  
+    def self.all_plants
+      all.each_with_index do |plant, index|
+        puts "#{index+1}. #{plant.name}"
+      end
+    end
+  end
